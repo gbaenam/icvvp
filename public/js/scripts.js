63101dfbcad15be1b4bd900214492eb7f649f1df
German Baena <germanb66@gmail.com>
2020-10-27 14:12:40 -0500

    Se finaliza la plantilla basica del header con la animacion del boton de hamburguesa

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,31 +1,16 @@
 
-// Hamburger button animation
-const   burguerButton = document.querySelector('.burguer-button'),
+const   header = document.getElementById('main-header'),
+        burguerButton = document.querySelector('.burguer-button'),
         burguerLine = document.querySelector('.burguer-button__line'),
         nav = document.getElementById('main-nav')
 
 
+// Hamburger button animation
 burguerButton.addEventListener('click', () => {
     burguerLine.classList.toggle('cruz')
     // Código para mover el nav
 })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const header = document.getElementById('main-header')
-// const nav = document.getElementById('main-nav')
-
+// Inserting the nav into the header
 // header.appendChild(nav)
 
