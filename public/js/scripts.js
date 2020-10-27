6f217901020eec554b995271b31095120e481f4a
German Baena <germanb66@gmail.com>
2020-10-27 13:32:45 -0500

    Se actualiza el codigo de _burger-button.scss

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,29 @@
+
+// Hamburger button animation
+const   burguerButton = document.querySelector('.burguer-button'),
+        burguerLine = document.querySelector('.burguer-button__line'),
+        nav = document.getElementById('main-nav')
+
+
+burguerButton.addEventListener('click', () => {
+    burguerLine.classList.toggle('cruz')
+    // Código para mover el nav
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // const header = document.getElementById('main-header')
 // const nav = document.getElementById('main-nav')
 
